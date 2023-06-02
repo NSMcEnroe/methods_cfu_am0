8e712e645010a81ba78a4fcdca8e771b30da97c8
Nicholas McEnroe <nsmcenro@gmail.com>
2023-06-02 08:14:20 -0400

    Fix ansers to challenge 1 and 2

--- final_practice.rb
+++ final_practice.rb
@@ -6,6 +6,25 @@
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
+def road_rage
+    return "Get off the road, jerk!"
+end
+
+puts road_rage
+puts road_rage
+
+
+road_rage_outburst = "Get off the road, jerk!"
+
+puts road_rage_outburst
+
+# What is the return value of your method?
+#The return value of my method is "Happy Holidays, #{name}!  My understanding of it is that this the output provided from the method when given appropriate inputs "name".
+# How many arguments did you pass your method?
+# I passed two arguments through my method.
+
+# 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
+
 def holiday_greeting(name)
     return "Happy Holidays, #{name}!"
 end
@@ -20,15 +39,13 @@ puts mom_holiday_greeting
 puts santa_holiday_greeting
 
 # What is the return value of your method?
+#The return value of my method is "Happy Holidays, #{name}!  My understanding of it is that this the output provided from the method when given appropriate inputs "name".
+
 # How many arguments did you pass your method?
+# I passed two arguments through my method.
 
-
-# 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
 # What data type was your argument(s)?
-
+# The data type was a string.
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
